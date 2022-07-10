9127ad375f505039fb0c5e2d7bc6368c9d2d7f40
aditya <priyadarshiaditya46@gmail.com>
2022-07-10 13:15:08 +0530

    till address bar, menu bar

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-let topRow = document.querySelector(".top_row");
-let leftCol = document.querySelector(".left_col");
-let grid = document.querySelector(".grid");
-// left column ke cells
-for (let i = 1; i <= 100; i++) {
-    // create a cell
-    let div = document.createElement("div");
-    div.setAttribute("class", "cell");
-    div.textContent = i;
-    leftCol.appendChild(div);
+// address bar implementation
+let cells = document.querySelectorAll(".grid .cell");
+let addressBar = document.querySelector(".address_bar");
+// 1. cell eventlistener -> on click
+for (let i = 0; i < cells.length; i++) {
+    // 2. when a cell is clicked -> element
+    cells[i].addEventListener("click", function () {
+        // console.log("Event happend");
+        let cCell = cells[i];
+        console.log(cCell);
+        // 3. get rid,cid-> address me convert
+        let rid = Number(cCell.getAttribute("rid"));
+        let cid = Number(cCell.getAttribute("cid"));
+        let address = String.fromCharCode(cid + 65) + (rid + 1);
+        console.log(address);
+        // 4.  put it into address bar
+        addressBar.value = address;
+    })
 }
-// top row ke cells
-for (let i = 1; i <= 26; i++) {
-    // create a cell
-    let div = document.createElement("div");
-    div.setAttribute("class", "cell");
-    div.textContent = String.fromCharCode(i + 64);
-    topRow.appendChild(div);
-}
-// grid 
-for (let i = 1; i <= 100; i++) {
-    let row = document.createElement("div");
-    row.setAttribute("class", "row");
-    for (let j = 1; j <= 26; j++) {
-        let col = document.createElement("div");
-        col.setAttribute("class", "cell");
-        col.textContent = `${String.fromCharCode(64 + j)} - ${i}`
-        row.appendChild(col);
-    }
-    grid.appendChild(row);
-}
+
